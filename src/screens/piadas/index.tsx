e04e560695637bd545f507295405d5bbbada856b
Manoel Iranilson <manoelvieirafilho0@gmail.com>
2022-06-14 21:25:11 -0300

    Organizando design

--- src/screens/piadas/index.tsx
+++ src/screens/piadas/index.tsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
-import {Alert, Text, TouchableOpacity, View} from 'react-native';
+import {Alert, Image, Text, TouchableOpacity, View} from 'react-native';
 import {Ipiadas, Jokes} from '../home';
-import {Card, Container, Piada} from './styles';
+import {Card, Container, Emotions, Piada} from './styles';
 import firestore from '@react-native-firebase/firestore';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Like from '../../assets/icons/like2.svg';
 
 interface IIPiadas {
   data: Jokes;
@@ -14,25 +14,51 @@ const Piadas: React.FC<IIPiadas> = ({data}) => {
   function Enviar(data: any) {
     setId(data.id);
     console.log(id);
+    console.log(data.setup);
     firestore().collection('piada').add({
       id,
       create_at: firestore.FieldValue.serverTimestamp(),
     });
   }
 
+  function ShowPiada() {
+    const [show, setShow] = useState(0);
+
+    if (show == 0) {
+      return (
+        <View>
+          <Text>da</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View>
+          <Text>ggg</Text>
+        </View>
+      );
+    }
+  }
+
   return (
     <Container>
       <TouchableOpacity onPress={() => Enviar(data)}>
         <Card>
-          <Text>{id}</Text>
-          <Text>{data.id}</Text>
           <View>
             <Piada>{data.setup}</Piada>
           </View>
-          <Text>{data.delivery}</Text>
-          <View>
-            <Icon name="users" size={60} color="#900" />
-          </View>
+          <TouchableOpacity onPress={() => ShowPiada()}></TouchableOpacity>
+          <Emotions>
+            <View style={{marginRight: 30, marginTop: 10}}>
+              <TouchableOpacity>
+                <Image source={require('../../assets/icons/dislike.png')} />
+              </TouchableOpacity>
+            </View>
+            <View>
+              <TouchableOpacity>
+                <Image source={require('../../assets/icons/gostar.png')} />
+              </TouchableOpacity>
+            </View>
+          </Emotions>
         </Card>
       </TouchableOpacity>
     </Container>
--- src/screens/piadas/styles.ts
+++ src/screens/piadas/styles.ts
@@ -4,17 +4,29 @@ import styled from 'styled-components';
 export const Container = styled(View)`
   flex: 1;
   padding: 10px;
+  background-color: #dcdcdc;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Card = styled(View)`
+  padding: 20px;
   height: 200px;
+  width: 350px;
   align-items: center;
   justify-content: center;
   border-radius: 20px;
-  background-color: gray;
+  background-color: white;
+  elevation: 10;
 `;
 
 export const Piada = styled(Text)`
   font-size: 20px;
-  color: white;
+  color: black;
+`;
+
+export const Emotions = styled(View)`
+  margin-left: 170px;
+  flex-direction: row;
+  padding: 22px;
 `;
