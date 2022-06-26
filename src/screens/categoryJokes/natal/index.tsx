46f37127e979ccccf3554dd9c6cc0c48ed7618d6
Manoel Iranilson <manoelvieirafilho0@gmail.com>
2022-06-25 22:09:03 -0300

    Resolvendo bug de pre-renderização

--- src/screens/categoryJokes/natal/index.tsx
+++ src/screens/categoryJokes/natal/index.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList, View, Text} from 'react-native';
+import {FlatList, View, Text, ActivityIndicator} from 'react-native';
 import api from '../../../services/api';
 import {Jokes} from '../../home';
 import Piadas from '../../piadas';
@@ -11,25 +11,33 @@ export interface Ipiadas {
 }
 const Natal: React.FC = () => {
   const [piadas, setPiadas] = useState<Ipiadas>();
-
+  const [load, setLoad] = useState(false);
   useEffect(() => {
     async function Apiget() {
       const response = await api.get('/joke/Christmas?amount=10');
       setPiadas(response.data);
       console.log(response);
+      setLoad(true);
     }
     Apiget();
   }, []);
-  return (
-    <View>
-      <Title>Christmas</Title>
-      <FlatList
-        data={piadas?.jokes}
-        // keyExtractor={item => item}
-        renderItem={({item, index}) => <Piadas key={index} data={item} />}
-      />
-    </View>
-  );
+  if (load === false) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator color="#FFD700" size={40} />
+      </View>
+    );
+  } else {
+    return (
+      <View>
+        <FlatList
+          data={piadas?.jokes}
+          // keyExtractor={item => item}
+          renderItem={({item, index}) => <Piadas key={index} data={item} />}
+        />
+      </View>
+    );
+  }
 };
 
 export default Natal;
